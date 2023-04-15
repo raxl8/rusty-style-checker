b4738280fd34e8b45b7740c100855dfd19cc7f36
raxl8 <32034076+raxl8@users.noreply.github.com>
2023-04-15 15:33:02 +0200

    feat(rules): add C-F8 rule

---
+++ src/rules/f8.rs
@@ -0,0 +1,32 @@
+use clang::token::TokenKind;
+
+use crate::file::{SourceFile, block::Block};
+
+pub struct RuleF8;
+
+fn process_blocks(source_file: &SourceFile, block: &Block, depth: u32) {
+    if depth > 10 {
+        return;
+    }
+    for token in block.tokens.iter() {
+        if token.kind == TokenKind::Comment {
+            println!(
+                "{}:{}: C-F8 Violation",
+                source_file.path, token.location.line
+            );
+        }
+    }
+    for child in block.children.iter() {
+        process_blocks(source_file, child, depth + 1);
+    }
+}
+
+impl super::Rule for RuleF8 {
+    fn analyze(&self, source_file: &SourceFile) {
+        for func in source_file.functions.iter() {
+            if let Some(block) = func.block.as_ref() {
+                process_blocks(source_file, block, 0);
+            }
+        }
+    }
+}
