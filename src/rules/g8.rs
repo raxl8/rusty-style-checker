93d66de14e7ef8fac011544bf69da94cca6d5369
raxl8 <32034076+raxl8@users.noreply.github.com>
2024-06-27 21:46:01 +0200

    feat(rules): add C-G8 rule

---
+++ src/rules/g8.rs
@@ -0,0 +1,29 @@
+use crate::file::SourceFile;
+
+pub struct RuleG8;
+
+fn get_last_empty_lines(lines: &Vec<String>) -> Vec<String> {
+    let mut empty_lines: Vec<String> = Vec::new();
+
+    for line in lines.iter().rev() {
+        if line.trim().is_empty() {
+            empty_lines.push(line.clone());
+        } else {
+            break;
+        }
+    }
+
+    empty_lines.reverse();
+    empty_lines
+}
+
+impl super::Rule for RuleG8 {
+    fn analyze(&self, source_file: &SourceFile) {
+        let mut empty_lines = get_last_empty_lines(&source_file.contents);
+        empty_lines.pop();
+        for i in 0..empty_lines.len() {
+            let line_number: usize = source_file.contents.len() - empty_lines.len() + i;
+            println!("{}:{}: C-G8 Violation", source_file.path, line_number);
+        }
+    }
+}
