1407d3194c807461d6cc67f7be511a26973dd6ca
raxl8 <32034076+raxl8@users.noreply.github.com>
2024-06-30 21:12:06 +0200

    feat(rules/g8): support leading empty lines

--- src/rules/g8.rs
+++ src/rules/g8.rs
@@ -1,6 +1,24 @@
-use crate::{file::{SourceFile, FileKind}, reporter::Reporter};
+use crate::{
+    file::{FileKind, SourceFile},
+    reporter::Reporter,
+};
 
 pub struct RuleG8;
+
+fn get_first_empty_lines(lines: &Vec<String>) -> Vec<String> {
+    let mut empty_lines: Vec<String> = Vec::new();
+
+    for line in lines.iter() {
+        if line.trim().is_empty() {
+            empty_lines.push(line.clone());
+        } else {
+            break;
+        }
+    }
+
+    empty_lines.reverse();
+    empty_lines
+}
 
 fn get_last_empty_lines(lines: &Vec<String>) -> Vec<String> {
     let mut empty_lines: Vec<String> = Vec::new();
@@ -24,10 +42,19 @@ impl super::Rule for RuleG8 {
             _ => return,
         }
 
-        let mut empty_lines = get_last_empty_lines(&source_file.contents);
-        empty_lines.pop(); // Remove the last empty line as it falls under C-A3
-        for i in 0..empty_lines.len() {
-            let line_number: usize = source_file.contents.len() - empty_lines.len() + i;
+        let first_empty_lines = get_first_empty_lines(&source_file.contents);
+        let mut last_empty_lines = get_last_empty_lines(&source_file.contents);
+        last_empty_lines.pop(); // Remove the last empty line as it falls under C-A3
+        for (i, _) in first_empty_lines.iter().enumerate() {
+            let line_number: usize = i + 1;
+            reporter.report(
+                source_file.path.clone(),
+                Some(line_number as u32),
+                "C-G8 Violation",
+            );
+        }
+        for (i, _) in last_empty_lines.iter().enumerate() {
+            let line_number: usize = source_file.contents.len() - last_empty_lines.len() + i;
             reporter.report(
                 source_file.path.clone(),
                 Some(line_number as u32),
